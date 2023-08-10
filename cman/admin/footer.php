941394e2e00e8392a1488f370dc90f068b4851e5
Bernard <elbernardprofessional@gmail.com>
2023-08-10 17:38:41 +0000

    Trying to host

---
+++ cman/admin/footer.php
@@ -0,0 +1,9 @@
+<div class="setfooter">
+		<footer >
+           <p align="center"><h3 align="center">&copy; St. William Chaplaincy AAMUSTED-K <?php
+ $date = new DateTime();
+ echo $date->format(' d M Y');
+ ?> </h3></p>
+</div>
+        <footer>
+</div>
